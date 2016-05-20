8b5f424ba1df30bf54e4ead9b63574dcf3550232
andreea-mihai <mihaiandreea07@gmail.com>
2016-05-20 08:10:21 +0300

    col

---
+++ PracticalTest02/src/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,94 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.network.ServerThread;
+
+public class PracticalTest02MainActivity extends Activity {
+	
+	// Server widgets
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private EditText cityEditText = null;
+    private Spinner informationTypeSpinner = null;
+    private Button getWeatherForecastButton = null;
+    private TextView weatherForecastTextView = null;
+
+    private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
+	
+
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverPortEditText.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Server port should be filled!",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            serverThread = new ServerThread(Integer.parseInt(serverPort));
+            if (serverThread.getServerSocket() != null) {
+                serverThread.start();
+            } else {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not creat server thread!");
+            }
+
+        }
+    }
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_practical_test02_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.practical_test02_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+    @Override
+    protected void onDestroy() {
+      if (serverThread != null) {
+        serverThread.stopThread();
+      }
+      super.onDestroy();
+    }
+}
